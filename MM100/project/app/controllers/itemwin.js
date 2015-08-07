8bec714a3954c0b5187d376de2659a103ac161e9
Ricardo Alcocer <alco@ricardoalcocer.com>
2015-08-07 14:37:21 -0700

    fixed type

--- MM100/project/app/controllers/itemwin.js
+++ MM100/project/app/controllers/itemwin.js
@@ -28,8 +28,8 @@ function togglePlayStop(evt){
 		sound.pause();
 	}else{
 		// don't be alarmed if you see this error on Android:
-		// MediaPlayer: Should have subtitle controller already set
-		// It's a known issue but completele harmless: https://jira.appcelerator.org/browse/TIMOB-16134
+		// "MediaPlayer: Should have subtitle controller already set"
+		// It's a known issue but completely harmless: https://jira.appcelerator.org/browse/TIMOB-16134
 		$.playstop.image="/square.png";
 		sound.play();
 	}
